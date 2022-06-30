d4bc077d94ca151095898fd43b7c021986927cb1
Антонов Антон Владимирович <antonov@ibs.ru>
2022-06-30 18:00:50 +0300

    Тест
    Тест

--- generator/test-dockerfile/start.sh
+++ generator/test-dockerfile/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 /opt/1cv8/x86_64/8.3.21.1302/ras cluster --port=${RAS_PORT} $RAS_CLUSTER_ADDRESS &
-/opt/1cv8/x86_64/8.3.21.1302/ragent \
+echo ${SRV1CV8_RANGE}
+echo /opt/1cv8/x86_64/8.3.21.1302/ragent \
                         -d ${SRV1CV8_DATA} \
                         -port ${SRV1CV8_PORT} \
                         -regport ${SRV1CV8_REGPORT} \
