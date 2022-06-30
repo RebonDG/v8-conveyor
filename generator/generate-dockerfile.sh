2d042b5d212bed28ab92417b1d67bff03166cc8f
Антонов Антон Владимирович <antonov@ibs.ru>
2022-07-01 01:06:05 +0300

    Первая версия скрипта по генерации докерфайла

---
+++ generator/generate-dockerfile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+while getopts v: flag
+do
+    case "${flag}" in
+        v) version=${OPTARG};;
+    esac
+done
+
+if [ $OPTIND -eq 1 ]; then 
+    echo "USAGE: ./generate-dockerfile.sh -v [1C_VERSION]";
+    echo "  -v [1C_VERSION] -- version of 1C"
+    echo "EXAMPLE:"
+    echo "  ./generate-dockerfile.sh -v 8.3.21.1302"
+    exit 1;
+fi
+
+if [ ! -d "./out" ]; then mkdir ./out fi
+if [ ! -d "./out/$version"]; then mkdir ./out/$version fi
+
+ONEC_VERSION=$version envsubst < docker-compose.yml.template >> ./out/$version/docker-compose.yml
+ONEC_VERSION=$version envsubst < Dockerfile.template >> ./out/$version/Dockerfile
