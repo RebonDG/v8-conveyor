078929c1fbdbaec6c10269d69a679694a0156420
Антонов Антон Владимирович <antonov@ibs.ru>
2022-06-30 18:09:36 +0300

    Добавлены индикаторы запуска

--- generator/test-dockerfile/start.sh
+++ generator/test-dockerfile/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+echo "Запуск RAS"
 /opt/1cv8/x86_64/8.3.21.1302/ras cluster --port=${RAS_PORT} $RAS_CLUSTER_ADDRESS &
-echo ${SRV1CV8_RANGE}
-echo /opt/1cv8/x86_64/8.3.21.1302/ragent \
+echo "Запуск 1С"
+/opt/1cv8/x86_64/8.3.21.1302/ragent \
                         -d ${SRV1CV8_DATA} \
                         -port ${SRV1CV8_PORT} \
                         -regport ${SRV1CV8_REGPORT} \
